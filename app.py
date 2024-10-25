9cb00c5b19164e819b19e8b583031a4c5c3f47a2
jonfodi <masterfodi@gmail.com>
2024-10-24 22:18:02 -0400

    change port

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hey"
+    return "oh hey"
 
 @app.route('/trigger')
 def trigger():
     return {"status": True}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
